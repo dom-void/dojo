dde8a4f230c85a45e080f81c16f0aebe0b7a3021
dom void <doe@onet.pl>
2017-04-13 22:56:30 +0200

    styled buttons prepared to do some magic

--- timer/timer.js
+++ timer/timer.js
@@ -5,22 +5,43 @@ document.addEventListener('DOMContentLoaded', function () {
     var minutes = seconds * 60;
     var hours = minutes * 60;
     var halfDays = hours * 12;
-    var days = 2*halfDays;
+    var days = 2 * halfDays;
 
     var canvas = document.getElementById("clock");
     var context = canvas.getContext("2d");
 
-    function Dial(innerRad,outerRad,color) {
+    function Time() {
+        this.actualHours;
+        this.actualMinutes;
+        this.actualSeconds;
+    }
+    Time.prototype.timeGet = function () {
+        var date = new Date();
+        var timeZoneOffset = date.getTimezoneOffset() * minutes;
+        var actualTime = date.getTime() - timeZoneOffset; // counting from 1 Jan 1970
+        return actualTime;
+    }
+    Time.prototype.setTimeParameters = function () {
+        var actualTime = this.timeGet();
+        var fullDaysInActualTime = Math.floor(actualTime / halfDays) * halfDays;
+        var daylySeconds = actualTime - fullDaysInActualTime;
+        this.actualHours = (daylySeconds / hours).toFixed(2);
+        this.actualMinutes = ((daylySeconds - Math.floor(this.actualHours) * hours) / minutes).toFixed(2);
+        this.actualSeconds = ((daylySeconds - ((Math.floor(this.actualHours) * hours) + Math.floor(this.actualMinutes) * minutes)) / seconds).toFixed(2);
+    }
+    var time = new Time();
+
+    function Dial(innerRad, outerRad, color) {
         this.centerX = 150;
         this.centerY = 150;
         this.innerRadius = innerRad;
         this.outerRadius = outerRad;
         this.angleOffset = -.5 * Math.PI;
-        this.angleStart = (function() { return this.angleOffset; })();
+        this.angleStart = (function () { return this.angleOffset; })();
         this.angleEnd = 0;
         this.color = color;
     }
-    Dial.prototype.draw = function (angleStart,angleEnd) {
+    Dial.prototype.draw = function (angleStart, angleEnd) {
         this.angleStart = angleStart;
         this.angleEnd = angleEnd;
         context.beginPath();
@@ -35,47 +56,39 @@ document.addEventListener('DOMContentLoaded', function () {
     var minutesColor = 'rgba(0,0,255,.5)';
     var secondsColor = 'rgba(255,255,0,.5)';
 
-    var hoursDial = new Dial(60,140,hoursColor);
-    var minutesDial = new Dial(40,120,minutesColor);
-    var secondsDial = new Dial(20,100,secondsColor);
+    var hoursDial = new Dial(60, 140, hoursColor);
+    var minutesDial = new Dial(40, 120, minutesColor);
+    var secondsDial = new Dial(20, 100, secondsColor);
 
-    window.requestAnimationFrame = (function () {
-        return window.requestAnimationFrame ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            function (callback) {
-                window.setTimeout(callback, 1000 / 60);
-            };
-    })();
+    // window.requestAnimationFrame = (function () {
+    //     return window.requestAnimationFrame ||
+    //         window.webkitRequestAnimationFrame ||
+    //         window.mozRequestAnimationFrame ||
+    //         window.oRequestAnimationFrame ||
+    //         window.msRequestAnimationFrame ||
+    //         function (callback) {
+    //             window.setTimeout(callback, 1000 / 60);
+    //         };
+    // })();
+
+    window.requestAnimationFrame = function (callback) {
+        window.setTimeout(callback, 1000 / 1);
+    };
 
     window.addEventListener('load', function () {
         window.requestAnimationFrame(drawLoop);
     }, false);
 
-    function time() {
-        var date = new Date();
-        var timeZoneOffset = date.getTimezoneOffset()*minutes;
-        var actualTime = date.getTime()-timeZoneOffset; // counting from 1 Jan 1970
-        return actualTime;
-    }
-
     function drawLoop() {
-        var actualTime = time();
-        var fullDaysInActualTime = Math.floor(actualTime / halfDays) * halfDays;
-        var daylySeconds = actualTime - fullDaysInActualTime;
-        var actualHours = daylySeconds / hours;
-        var actualMinutes = (daylySeconds - Math.floor(actualHours) * hours) / minutes;
-        var actualSeconds = (daylySeconds - ((Math.floor(actualHours) * hours) + Math.floor(actualMinutes) * minutes)) / seconds;
+        time.setTimeParameters();
         var angleStart = -.5 * Math.PI;
-        var secondsAngleEnd = secondsDial.angleStart + actualSeconds * (2 * Math.PI / 60);
-        var minutesAngleEnd = minutesDial.angleStart + actualMinutes * (2 * Math.PI / 60);
-        var hoursAngleEnd = minutesDial.angleStart + actualHours * (2 * Math.PI / 12);
+        var secondsAngleEnd = secondsDial.angleStart + time.actualSeconds * (2 * Math.PI / 60);
+        var minutesAngleEnd = minutesDial.angleStart + time.actualMinutes * (2 * Math.PI / 60);
+        var hoursAngleEnd = minutesDial.angleStart + time.actualHours * (2 * Math.PI / 12);
         context.clearRect(0, 0, canvas.width, canvas.height);
-        hoursDial.draw(angleStart,hoursAngleEnd);
-        minutesDial.draw(angleStart,minutesAngleEnd);
-        secondsDial.draw(angleStart,secondsAngleEnd);
+        hoursDial.draw(angleStart, hoursAngleEnd);
+        minutesDial.draw(angleStart, minutesAngleEnd);
+        secondsDial.draw(angleStart, secondsAngleEnd);
         window.requestAnimationFrame(drawLoop);
     }
 })
