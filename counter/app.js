f2f7cc3d44131513a946d1c8b87a8a5e8bf71736
dom void <doe@onet.pl>
2021-04-13 14:36:02 +0200

    added reset button and render

--- counter/app.js
+++ counter/app.js
@@ -3,16 +3,29 @@ let counter = {
     set add(x) {
         this._state += x;
     },
+    reset() {
+        this._state = 0;
+    },
     get state() {
         return this._state;
+    },
+    render() {
+        return `${this._state}`
     }
 };
 
 let app = document.getElementById("app");
 let button = document.getElementById("button");
+let reset = document.getElementById("reset");
+
 button.addEventListener("click", () => {
     counter.add = 1;
-    return (app.innerHTML = `${counter.state}`);
+    app.innerHTML = counter.render();
 });
+
+reset.addEventListener("click", () => {
+    counter.reset();
+    app.innerHTML = counter.render();
+})
 
 app.innerHTML = `${counter.state}`;
