3ec910b8448e2cf1cd3f5ddfb16363403a788e68
dom void <doe@onet.pl>
2021-04-19 20:47:49 +0200

    searches on input

--- countries/app.js
+++ countries/app.js
@@ -1,5 +1,5 @@
 function loadData(countryName) {
-    return fetch(`https://restcountries.eu/rest/v2/name/${countryName}`) //fetch zwraca nam obietnicę
+    return fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
         .then((res) => {
             //then też zwraca nam obietnicę
             if (res.ok) {
@@ -10,11 +10,21 @@ function loadData(countryName) {
         });
 }
 
-loadData("polan")
-    .then((res) => {
-        console.log(res[0].name);
-        document.querySelector('#app').insertAdjacentHTML("afterbegin", `<img src=${res[0].flag} width=200 style="border: 1px solid gray;">`);
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+async function renderData(value) {
+    loadData(value)
+        .then((res) => {
+            console.log(res[0].name);
+            document.querySelector('#app').insertAdjacentHTML("afterbegin", `<img src=${res[0].flag} width=200 style="border: 1px solid gray;">`);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
+
+const search = document.querySelector('#search');
+search.addEventListener('input', function (val) {
+    console.log(this.value.length);
+    if (this.value.length > 2) {
+        renderData(this.value);
+    }
+});
